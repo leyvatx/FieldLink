86feaf81013b697b8c861469a71e20d8e7dc7203
leyvatx <tunzur@gmail.com>
2026-02-03 22:57:50 -0800

    fix: :art: Se cambió la paginación en las consultas de GET.

--- api/config/settings.py
+++ api/config/settings.py
@@ -102,8 +102,6 @@ AUTH_USER_MODEL = 'usuarios.User'
 # REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
 }
 
 # Swagger/OpenAPI
